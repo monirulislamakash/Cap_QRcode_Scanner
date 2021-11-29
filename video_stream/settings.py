c0f9a347f61c16113c54da3f1c2bc370464801c6
Ak@sh <aaj7731@gmail.com>
2021-11-29 17:34:28 +0600

    Update

--- video_stream/settings.py
+++ video_stream/settings.py
@@ -16,7 +16,7 @@ import os
 import os 
 import django_heroku
 import dj_database_url
-from decouple import config
+from decouple import Config
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
