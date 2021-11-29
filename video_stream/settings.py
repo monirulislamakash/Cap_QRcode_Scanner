35b478fda17fb3a255a984918e2f09f9cc270fa3
Ak@sh <aaj7731@gmail.com>
2021-11-29 18:16:25 +0600

    Update

--- video_stream/settings.py
+++ video_stream/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '1lxk3a6m+6c-@mr5i3k6(4sv73-f@s=o*j2@#^)e^^i-px4!kj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://capqrs.herokuapp.com/"]
+ALLOWED_HOSTS = ["capqrs.herokuapp.com"]
 
 
 # Application definition
