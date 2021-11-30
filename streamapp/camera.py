af72efb7ee128919e0a078bcd3d1f95a8c976910
Ak@sh <aaj7731@gmail.com>
2021-11-30 22:52:50 +0600

    siam guru coder

--- streamapp/camera.py
+++ streamapp/camera.py
@@ -2,53 +2,77 @@ import cv2,os,urllib.request
 import numpy as np
 from django.conf import settings
 import datetime
+import math
 from django.shortcuts import render
-
+import pandas as pd
+import os
 
 class VideoCamera(object):
+
+	kernel = np.ones((5,5),np.uint8)
+	qrcode_detectoin=cv2.QRCodeDetector()
+	dic={'qr_code':[]}
+
 	def __init__(self):
-		self.video = cv2.VideoCapture('http://127.0.0.1:8000/static/stor/this.mp4')
+		self.video = cv2.VideoCapture(0)
 
 	def __del__(self):
 		self.video.release()
 
-	def get_frame(self):
-		success, image = self.video.read()
-		# We are using Motion JPEG, but OpenCV defaults to capture raw images,
-		# so we must encode it into JPEG in order to correctly display the
-		# video stream.
-		frame_flip = cv2.flip(image,1)
-		ret, jpeg = cv2.imencode('.jpg', frame_flip)
-		return jpeg.tobytes()
+	def getAngle(self, a, b, c):
+		ang = math.degrees(math.atan2(c[1] - b[1], c[0] - b[0]) - math.atan2(a[1] - b[1], a[0] - b[0]))
+		return ang if ang < 0 else ang
 
-
-class IPWebCam(object):
-	def __init__(self):
-		self.video = cv2.VideoCapture('static/Funny CCTV VIDEOS _ funny videos _ Comedy videos _ Comedy funny videos.mp4')
-
-	def __del__(self):
-		cv2.destroyAllWindows()
 
 	def get_frame(self):
-		success, image = self.video.read()
-		# We are using Motion JPEG, but OpenCV defaults to capture raw images,
-		# so we must encode it into JPEG in order to correctly display the
-		# video stream.
-		frame_flip = cv2.flip(image,1)
-
-		ret, jpeg = cv2.imencode('.jpg', frame_flip)
-		return jpeg.tobytes()
-
+		#success, image = self.video.read()
+		img=cv2.imread('1.jpg')
 		
-class LiveWebCam(object):
-	def __init__(self):
-		self.url = cv2.VideoCapture("rtsp://admin:Mumbai@123@203.192.228.175:554/")
+		hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+		lower_blue = np.array([0,0,168])
+		upper_blue = np.array([172,111,255])
+		mask = cv2.inRange(hsv, lower_blue, upper_blue)
+		res = cv2.bitwise_and(img, img, mask= mask)
 
-	def __del__(self):
-		cv2.destroyAllWindows()
+		erosion = cv2.erode(res,self.kernel,iterations =1)
 
-	def get_frame(self):
-		success,imgNp = self.url.read()
-		resize = cv2.resize(imgNp, (640, 480), interpolation = cv2.INTER_LINEAR) 
-		ret, jpeg = cv2.imencode('.jpg', resize)
+		gray= cv2.cvtColor(erosion, cv2.COLOR_BGR2GRAY)
+
+		opening = cv2.morphologyEx(gray, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
+
+		contours0, hierarchy = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		cnumber = 0
+
+		for sl, cnt in enumerate(contours0):
+			x, y, w, h = cv2.boundingRect(cnt)
+			crop_img=img[y:y+h, x:x+w]
+
+			edges = cv2.Canny(crop_img, 40, 300)
+			circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 1000,
+			param1=60, param2=30, minRadius=0, maxRadius=0)
+
+			try:
+				for i in circles[0, :]:
+					cnumber +=1
+					cx = int(i[0])
+					cy = int(i[1])
+					r = int(i[2])
+					angle = int(self.getAngle((x, y), (x + w, y + h), (x+h, x)))
+					if (angle >= 40 and angle <= 50) or (angle >= 220 and angle <= 230):
+						ret, thresh1 = cv2.threshold(crop_img, 170, 255, cv2.THRESH_BINARY)
+						decodedText, points, _ = self.qrcode_detectoin.detectAndDecode(thresh1)
+						#here is final output as string
+						img[y:y + h, x:x + w][:, :, 2] = 255
+						#file.writelines(f"{decodedText} \n")
+						self.dic['qr_code'].append(decodedText)
+						
+			except:
+				pass
+
+		df=pd.DataFrame(self.dic)
+		df.to_csv("output.csv")
+		cv2.imwrite('output.jpg', img)
+		os.remove('1.jpg')
+		ret, jpeg = cv2.imencode('.jpg', img)
 		return jpeg.tobytes()
+
--- streamapp/views.py
+++ streamapp/views.py
@@ -1,112 +1,26 @@
 from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
-from streamapp.camera import VideoCamera, IPWebCam, LiveWebCam
+from streamapp.camera import VideoCamera
 from .models import Cam_Configer,Empolys,Know,Unknow,CameraName,Cam_con
 import datetime
+import requests
 from django.utils import timezone
 from .forms import Show,ShowDate,ShowTime,Showun,Showund
 # Create your views here.
 
 
 def index(request):
-	cname=CameraName.objects.all()
-	now = datetime.datetime.now()
-	var={
-		"cname":cname,
-		"time":now,
-	}
-	return render(request,'streamapp/home.html',var)
-def show(request,id):
-	pk=Know.objects.get(pk=id)
-	show=Show(instance=pk)
-	showtime=ShowTime(instance=pk)
-	showdate=ShowDate(instance=pk)
-	prim='http://127.0.0.1:8000/static/stor/develop-a-professional-python-django-websites.jpg'
-	var={
-		'show':show,
-		'showtime':showtime,
-		'showdate':showdate,
-		'prim':prim,
-	}
-	return render(request,"streamapp/show.html",var)
-def showun(request,id):
-	pk=Unknow.objects.get(pk=id)
-	showtime=Showun(instance=pk)
-	showdate=Showund(instance=pk)
-	var={
-		'showtime':showtime,
-		'showdate':showdate
-
-	}
-	return render(request,"streamapp/show.html",var)
-
-def report(request):
+	mainurl=''
 	if request.method=="POST":
-		select1=request.POST.get("selector_known_or_gust")
-		select2=request.POST.get("selector_entry_exit")
-		if select1=="known":
-			status=Know.objects.all()
-			VAR={
-    		"status":status,
-    		}
-			return render(request,"streamapp/configuration.html",VAR)
+		url=request.POST.get('url')
+		mainurl=mainurl+url
+		print(mainurl)
+		r = requests.get(mainurl)
+		with open('1.jpg', 'wb') as f:
+			f.write(r.content)
+			return render(request,'streamapp/home.html')
+	return render(request,'streamapp/home.html')
 
-		elif select1=="gust":
-			status=Unknow.objects.all()
-			VAR={
-    		"status1":status,
-    		}
-			return render(request,"streamapp/configuration.html",VAR)
-		tfrom=request.POST.get("from")
-		t_to=request.POST.get("to")
-	return render(request,"streamapp/configuration.html")
-def configuration(request):
-	if request.method=="POST":
-		names=request.POST.get("name")
-		password=request.POST.get("password")
-		ipaddrss=request.POST.get("ipaddress")
-		cameraname=request.POST.get("cname")
-		fm=Cam_Configer(name=names,pasw=password,ip_address=ipaddrss,camera_number=cameraname)
-		fm.save()
-		return render(request,"streamapp/report.html",{"success":"Report Update successfully"})
-	return render(request,"streamapp/report.html")
-def configuration2(request):
-	if request.method=="POST":
-		names=request.POST.get("name")
-		password=request.POST.get("password")
-		ipaddrss=request.POST.get("ipaddress")
-		cameraname=request.POST.get("cname")
-		fm=Cam_con(Name=names,Password=password,Ip_address=ipaddrss,Camera_NO=cameraname)
-		fm.save()
-		return render(request,"streamapp/camera2.html",{"success":"Report Update successfully"})
-	return render(request,"streamapp/camera2.html")
-def addemp(request):
-	if request.method=="POST":
-		e_id=request.POST.get("id")
-		name=request.POST.get("name")
-		mobile=request.POST.get("mobile")
-		pic=request.FILES["img"]
-		try:
-			user=Empolys.objects.get(ids=e_id)
-			return render(request,"streamapp/addemp.html",{'error':"User ID already exists"})
-		except Empolys.DoesNotExist:
-			efm=Empolys(ids=e_id,name=name,mobile_no=mobile,image=pic)
-			efm.save()
-			return render(request,"streamapp/addemp.html",{"sucess":"your information is uploaded successfully"})
-	return render(request,"streamapp/addemp.html")
-def emplist(request):
-	emplist=Empolys.objects.all()
-	htmlvar={
-		'emplist':emplist
-	}
-	return render(request,"streamapp/emplist.html",htmlvar)
-
-
-
-def viewemp(request,id):
-	ids=id
-	name=Empolys.objects.filter(ids=ids)
-	return render(request,"streamapp/viewemp.html",{'name':name})
 
 def gen(camera):
 	while True:
@@ -118,12 +32,6 @@ def gen(camera):
 def video_feed(request):
 	return StreamingHttpResponse(gen(VideoCamera()),
 					content_type='multipart/x-mixed-replace; boundary=frame')
-
-
-def webcam_feed(request):
-	return StreamingHttpResponse(gen(IPWebCam()),
-					content_type='multipart/x-mixed-replace; boundary=frame')
-
 
 '''def mask_feed(request):
 	return StreamingHttpResponse(gen(MaskDetect()),
